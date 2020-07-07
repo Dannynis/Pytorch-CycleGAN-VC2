2acf19c3ee17a5921901a6075d525e7b20b318de
Danny <dannynis1996@gmail.com>
2020-07-07 19:35:25 +0300

    switch to 16k because embedding was trained on 16k voxceleb

--- preprocess_training.py
+++ preprocess_training.py
@@ -20,10 +20,10 @@ def load_pickle_file(fileName):
 
 def preprocess_for_training(train_A_dir, train_B_dir, cache_folder):
     num_mcep = 48
-    sampling_rate = 44000
+    sampling_rate = 16000
     frame_period = 5.0
     n_frames = 128
-
+    print ('using {} sample rate'.format(sampling_rate))
     print("Starting to prepocess data.......")
     start_time = time.time()
 
